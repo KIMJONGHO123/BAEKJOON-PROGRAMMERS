7676f549cdae1495fdb27ae586e1960534245247
KIM <89973945+KIMJONGHO123@users.noreply.github.com>
2024-07-02 00:42:07 +0900

    [Bronze V] Title: 꼬마 정민, Time: 108 ms, Memory: 12836 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		long num1,num2,num3;
+		num1 = input.nextLong();
+		num2 = input.nextLong();
+		num3 = input.nextLong();
+		
+		System.out.println(num1+num2+num3);
+	}
+	
+}
