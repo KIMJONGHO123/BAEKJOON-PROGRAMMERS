146d8376ddf51d12cb638fdd51808b7f1e4d7c28
KIM <89973945+KIMJONGHO123@users.noreply.github.com>
2024-07-05 22:09:41 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17580 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,17 @@
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		char n;
+		n = input.next().charAt(0);
+		
+		int i = n;
+		
+		System.out.println(i);
+		
+		
+	}
+}
