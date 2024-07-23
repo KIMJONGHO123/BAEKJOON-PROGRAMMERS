50ecc1234d1ca7d4df50e160134e400e2eb4f42e
KIM <89973945+KIMJONGHO123@users.noreply.github.com>
2024-07-23 14:33:30 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.04 ms, Memory: 66.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        double total = (double)num1/num2;
+        double total_1 = total * 1000;
+
+        return (int)total_1;
+    }
+}
