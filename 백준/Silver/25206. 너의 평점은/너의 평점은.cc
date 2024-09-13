e07385e44ce44592881ff20b64b472931a291390
KIM <89973945+KIMJONGHO123@users.noreply.github.com>
2024-09-13 21:20:45 +0900

    [Silver V] Title: 너의 평점은, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.cc
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.cc
@@ -2,7 +2,7 @@
 
 int main() {
 
-	char subject[51],grade[3];  // 문자열 끝을 표시하는 널 문자까지 포함하려면 공간이 하나 더 필요함.
+	char subject[50],grade[3];  // 문자열 끝을 표시하는 널 문자까지 포함하려면 공간이 하나 더 필요함.
 	double credit,creditsum=0.0,gradescore,multiply=0.0,avg;
 
 	for (int i = 0; i < 20; i++)
