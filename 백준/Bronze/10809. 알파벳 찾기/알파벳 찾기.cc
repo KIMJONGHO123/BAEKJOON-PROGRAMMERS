5c7c9e497b0cb9e5b0ad55c90ec85ae47fc9e497
KIM <89973945+KIMJONGHO123@users.noreply.github.com>
2024-11-13 15:14:47 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
@@ -4,29 +4,27 @@
 int main() {
 
 	char s[101];
-	int arr[26] = {-1};
+	int arr[26];
 	scanf("%s", s);
-	int length = strlen(s);
-	
+
 	for (int i = 0; i < 26; i++)
 	{
 		arr[i] = -1;
 	}
+	int length = strlen(s);
 	for (int i = 0; i < length; i++)
 	{
-		int index = s[i] - 97;
+		int index = s[i] - 'a';
 		if (arr[index] == -1) {
+			
 			arr[index] = i;
+
 		}
-		
 	}
 
 	for (int i = 0; i < 26; i++)
 	{
 		printf("%d ", arr[i]);
 	}
-	
-
-
 	return 0;
 }
