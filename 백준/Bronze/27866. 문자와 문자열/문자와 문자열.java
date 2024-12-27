787cd5a59a1e387d32b96f9d36af9955002b6bb0
KIM <89973945+KIMJONGHO123@users.noreply.github.com>
2024-12-28 00:32:29 +0900

    [Bronze V] Title: 문자와 문자열, Time: 172 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -1,17 +1,11 @@
-import java.util.Iterator;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		
-		String n;
-		n = input.next();
-		int index;
-		index = input.nextInt()-1;
-		
-		System.out.println(n.charAt(index));
+		String s = input.next();
+		int n = input.nextInt();
+		System.out.println(s.charAt(n-1));
 		
 	}
-
 }
