9208403e1d748e27a8b1584b67758851c686c154
KIM <89973945+KIMJONGHO123@users.noreply.github.com>
2025-03-23 21:14:51 +0900

    [level 2] Title: 괄호 회전하기, Time: 122.97 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/README.md
+++ 프로그래머스/2/76502. 괄호 회전하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 회전하기 - 76502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502) 
 
 ### 성능 요약
 
-메모리: 86 MB, 시간: 59.11 ms
+메모리: 111 MB, 시간: 122.97 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 23일 15:52:52
+2025년 03월 23일 21:14:57
 
 ### 문제 설명
 
--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -2,49 +2,72 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         
-        int count =0;
-        for(int i=0; i<s.length();i++){
-            Stack <String> st = new Stack<>();
-            boolean answer = true;
-            String newS = s.substring(i)+s.substring(0,i);
-            for(int j=0; j < newS.length(); j++){
-                if(newS.charAt(j) == '['){
-                    st.push("[");
-                }else if(newS.charAt(j) == '{'){
-                    st.push("{");
-                }else if(newS.charAt(j) == '('){
-                    st.push("(");
-                }
+//         int count =0;
+//         for(int i=0; i<s.length();i++){
+//             Stack <String> st = new Stack<>();
+//             boolean answer = true;
+//             String newS = s.substring(i)+s.substring(0,i);
+//             for(int j=0; j < newS.length(); j++){
+//                 if(newS.charAt(j) == '['){
+//                     st.push("[");
+//                 }else if(newS.charAt(j) == '{'){
+//                     st.push("{");
+//                 }else if(newS.charAt(j) == '('){
+//                     st.push("(");
+//                 }
                 
-                else if(newS.charAt(j) == ']'){
-                    if(!st.isEmpty() && st.peek().equals("[")){
-                        st.pop();
-                    }else{
-                        answer = false;
-                        break;
-                    }
-                }else if(newS.charAt(j) == '}'){
-                    if(!st.isEmpty() && st.peek().equals("{")){
-                        st.pop();
-                    }else{
-                        answer = false;
-                        break;
-                    }
-                }else if(newS.charAt(j) == ')'){
-                    if(!st.isEmpty() && st.peek().equals("(")){
-                        st.pop();
-                    }else{
-                        answer = false;
-                        break;
-                    }
-                }
+//                 else if(newS.charAt(j) == ']'){
+//                     if(!st.isEmpty() && st.peek().equals("[")){
+//                         st.pop();
+//                     }else{
+//                         answer = false;
+//                         break;
+//                     }
+//                 }else if(newS.charAt(j) == '}'){
+//                     if(!st.isEmpty() && st.peek().equals("{")){
+//                         st.pop();
+//                     }else{
+//                         answer = false;
+//                         break;
+//                     }
+//                 }else if(newS.charAt(j) == ')'){
+//                     if(!st.isEmpty() && st.peek().equals("(")){
+//                         st.pop();
+//                     }else{
+//                         answer = false;
+//                         break;
+//                     }
+//                 }
                 
+//             }
+//             if(answer && st.isEmpty()){
+//                 count++;
+//             }
+//         }
+        
+//         return count;
+        int answer=0;
+        for(int i=0; i<s.length(); i++){
+            Stack <String> stack = new Stack<>();
+            String str = s.substring(i)+s.substring(0,i);
+            for(int j=0; j<str.length(); j++){
+                char ch = str.charAt(j);
+                if(stack.isEmpty()){
+                    stack.push(String.valueOf(ch));
+                }else if(ch == ']' && stack.peek().equals("[")){
+                    stack.pop();   
+                }else if(ch == '}' && stack.peek().equals("{")){
+                    stack.pop();
+                }else if(ch==')' && stack.peek().equals("(")){
+                    stack.pop();
+                }else{
+                    stack.push(String.valueOf(ch));
+                }
             }
-            if(answer && st.isEmpty()){
-                count++;
+            if(stack.isEmpty()){
+                answer++;
             }
         }
-        
-        return count;
-    }
+        return answer;
+     }
 }
