fe7b27f8283933014ce1991c5c16e7ea35450cfb
KIM <89973945+KIMJONGHO123@users.noreply.github.com>
2025-07-19 17:22:22 +0900

    [level 2] Title: 괄호 회전하기, Time: 50.02 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -2,72 +2,40 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         
-//         int count =0;
-//         for(int i=0; i<s.length();i++){
-//             Stack <String> st = new Stack<>();
-//             boolean answer = true;
-//             String newS = s.substring(i)+s.substring(0,i);
-//             for(int j=0; j < newS.length(); j++){
-//                 if(newS.charAt(j) == '['){
-//                     st.push("[");
-//                 }else if(newS.charAt(j) == '{'){
-//                     st.push("{");
-//                 }else if(newS.charAt(j) == '('){
-//                     st.push("(");
-//                 }
-                
-//                 else if(newS.charAt(j) == ']'){
-//                     if(!st.isEmpty() && st.peek().equals("[")){
-//                         st.pop();
-//                     }else{
-//                         answer = false;
-//                         break;
-//                     }
-//                 }else if(newS.charAt(j) == '}'){
-//                     if(!st.isEmpty() && st.peek().equals("{")){
-//                         st.pop();
-//                     }else{
-//                         answer = false;
-//                         break;
-//                     }
-//                 }else if(newS.charAt(j) == ')'){
-//                     if(!st.isEmpty() && st.peek().equals("(")){
-//                         st.pop();
-//                     }else{
-//                         answer = false;
-//                         break;
-//                     }
-//                 }
-                
-//             }
-//             if(answer && st.isEmpty()){
-//                 count++;
-//             }
-//         }
-        
-//         return count;
-        int answer=0;
-        for(int i=0; i<s.length(); i++){
-            Stack <String> stack = new Stack<>();
-            String str = s.substring(i)+s.substring(0,i);
-            for(int j=0; j<str.length(); j++){
-                char ch = str.charAt(j);
-                if(stack.isEmpty()){
-                    stack.push(String.valueOf(ch));
-                }else if(ch == ']' && stack.peek().equals("[")){
-                    stack.pop();   
-                }else if(ch == '}' && stack.peek().equals("{")){
-                    stack.pop();
-                }else if(ch==')' && stack.peek().equals("(")){
-                    stack.pop();
+        int count =0;
+        for(int j =0; j<s.length(); j++){
+            boolean check = false;
+            Stack<Character> stack = new Stack<>();
+            if(j != 0){
+                String a = s.substring(0,1);
+                String aa = s.substring(1,s.length());
+                s = aa.concat(a);
+            }
+            
+            for(int i=0; i< s.length(); i++){
+                check=false;
+                if(s.charAt(i)=='('||s.charAt(i)=='{'||s.charAt(i)=='['){
+                    stack.push(s.charAt(i));
                 }else{
-                    stack.push(String.valueOf(ch));
+                    if(!stack.isEmpty()){
+                        if(s.charAt(i) ==')'&& stack.peek()=='(' || s.charAt(i) =='}'&& stack.peek()=='{' || s.charAt(i) ==']'&& stack.peek()=='['){
+                             stack.pop();
+                             check=true;
+                        }
+                       
+                    }else{
+                        break;
+                    }
                 }
             }
-            if(stack.isEmpty()){
-                answer++;
+            if(check == true){
+                count++;
             }
+            if(!stack.isEmpty()){
+                return 0;
+            }
+
         }
-        return answer;
-     }
+        return count;
+    }
 }
