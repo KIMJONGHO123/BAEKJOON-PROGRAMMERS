ebb9451576e85ffbdc325640a85e2ff19628d96d
KIM <89973945+KIMJONGHO123@users.noreply.github.com>
2025-09-01 15:43:37 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 79.68 ms, Memory: 97.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
 
 ### 성능 요약
 
-메모리: 97.2 MB, 시간: 91.62 ms
+메모리: 97.8 MB, 시간: 79.68 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 01일 23:13:02
+2025년 09월 01일 15:43:34
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,45 +1,21 @@
 import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Map <String,Integer> map = new HashMap<>(); // participant 배열의 횟수
+        String s ="";
+       HashMap<String,Integer> hm = new HashMap<>();
         for(int i=0; i<participant.length; i++){
-            map.put(participant[i], map.getOrDefault(participant[i],0)+1); // 
+            hm.put(participant[i],hm.getOrDefault(participant[i],0)+1);
+            // participant안에 있는 원소의 갯수 구하기
         }
-        for(int j=0;j< completion.length;j++){
-            map.put(completion[j],map.get(completion[j])-1); //완주한 선수들의 value를 -1 해준다.
+        for(String n: completion){
+            hm.put(n,hm.get(n)-1); //완주한 사람 -1하기
         }
-        List<String> li = new ArrayList<>();  // 완주하지 못한 선수를 넣을 리스트
-        
-        for(int i=0; i<participant.length; i++){
-            if(map.get(participant[i]) !=0 ){ // value 값 중에서 0 이 아니면
-                li.add(participant[i]);       // li에 key를 집어넣는다.
+        for(String q : participant){
+            if(hm.get(q)!=0){
+                s = q;
+                break;
             }
         }
-        String s = li.get(0);
         return s;
-        
     }
 }
-
-// import java.util.*;
-// class Solution {
-//     public String solution(String[] participant, String[] completion) {
-//         Map <String,Integer> map = new HashMap<>(); // participant 배열의 횟수
-//         for(int i=0; i<participant.length; i++){
-//             map.put(participant[i], map.getOrDefault(participant[i],0)+1);
-//         }
-//         for(int j=0;j< completion.length;j++){
-//             map.put(completion[j],map.get(completion[j])-1); // 
-//         }
-
-        
-//         for(int i=0; i<participant.length; i++){
-//             if(map.get(participant[i]) !=0 ){ // value 값 중에서 0 이 아니면
-//                 return participant[i];
-//             }
-//         }
-
-
-//         return "";
-//     }
-// }
