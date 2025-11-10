cc70530f4ff3bba63d393d7419cf2a0b09d6266b
KIM <89973945+KIMJONGHO123@users.noreply.github.com>
2025-11-10 15:48:17 +0900

    [Bronze I] Title: 평균, Time: 116 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,35 +1,36 @@
-import java.util.Iterator;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		int t;
-		double total;
-		
-		
-		t = input.nextInt();
-		int [] score = new int[t];
-		for (int i = 0; i < t; i++) {
-			score[i] = input.nextInt();
-		}
-		
-		int max;
-		max = score[0];
-		for (int i = 0; i < t; i++) {
-			if (max < score[i]) {
-				max = score[i];
-			}
-		}
-		total = 0;
-		for (int i = 0; i < t; i++) {
-			double trans_score = ((double)score[i] / max *100);
-			total += trans_score;
-		}
-		
-		System.out.println(total/t);
-		
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bf.readLine());
+
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        double array[] = new double[n];
+
+        for(int i =0; i<n; i++){
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+
+        double max= array[0]; // max 값
+        for(int k=1; k<n; k++){
+            if(array[k]>max){
+                max = array[k];
+            }
+        }
+
+        double sum=0;
+        for(int j=0; j<n; j++){
+            sum += array[j]/max*100;
+        }
+        double result = sum/n;
+        System.out.print(result);
+
+    }
 
 }
