7c2e052bf8115f922d47af1a7cb705ea5c187905
KIM <89973945+KIMJONGHO123@users.noreply.github.com>
2026-03-03 23:20:41 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 107.80 ms, Memory: 97.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 97.8 MB, 시간: 79.68 ms
+메모리: 97.3 MB, 시간: 107.80 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 01일 15:43:34
+2026년 03월 03일 23:20:35
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,21 +1,25 @@
 import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String s ="";
-       HashMap<String,Integer> hm = new HashMap<>();
+        HashMap<String,Integer> hm = new HashMap();
+        String answer="";
+        
+        // HashMap에 입력
         for(int i=0; i<participant.length; i++){
             hm.put(participant[i],hm.getOrDefault(participant[i],0)+1);
-            // participant안에 있는 원소의 갯수 구하기
         }
-        for(String n: completion){
-            hm.put(n,hm.get(n)-1); //완주한 사람 -1하기
+        
+        for(String st: completion){
+            if(hm.containsKey(st)) hm.put(st,hm.get(st)-1);
         }
-        for(String q : participant){
-            if(hm.get(q)!=0){
-                s = q;
+        
+        for(String key:hm.keySet()){
+            if(hm.get(key) ==1){
+                answer = key;
                 break;
             }
         }
-        return s;
+        
+        return answer;
     }
 }
